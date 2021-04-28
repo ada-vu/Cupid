1b95f21144b7d08f5c086700ffbdccf40b2a4474
Ada Vu <vu.hanh.ada@gmail.com>
2021-04-28 19:50:07 -0400

    finished !help command embed message

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 
 const exampleEmbed = new Discord.MessageEmbed()
 	.setColor('#b171d1')
+	.attachFiles('images/cupid.png')
 	.setTitle(':question:\ Help Guide')
-	.setAuthor('Cupid Bot', 'https://images.unsplash.com/photo-1571172964533-d2d13d88ce7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80')
-	.setDescription(`Welcome to Cupid Bot! Being a cupid for your friends is really easy!\n\nFollow these commands below`)
-	.setThumbnail('https://images.unsplash.com/photo-1571172964533-d2d13d88ce7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80')
+	.setAuthor('Cupid Bot', 'attachment://cupid.png')
+	.setDescription(`Welcome to Cupid Bot! Be the cupid for your friends!\n\nFollow these commands below`)
+	.setThumbnail('attachment://cupid.png')
 	.addFields(
 		{ name: '\u200B', value: '\u200B' },
-		{ name: "Commands", value: `:heart:\ \`!match <username> <username>\` | Match your friends!\n\n :kissing_heart:\ \`!flirt <category GIF> <username>\` | Send a random categorized GIF to your crush!\n\n :question:\ \`!help\` | Gives you info on Cupid Bot` },
+		{ name: "Commands", value: `:heart:\ \`!match <username> <username>\` | Match your friends!\n\n :kissing_heart:\ \`!flirt <category GIF> <username>\` | Be brave and send a random categorized GIF to your crush!\n\n :question:\ \`!help\` | Gives you info on Cupid Bot` },
 		{ name: '\u200B', value: '\u200B' },
 		{ name: 'React!\ :wave:', value: 'React to the match messages how you feel about the couple being matched!', inline: true },
 		{ name: 'Cuteness Level %\ :heart_eyes:', value: 'After a few hours, Cupid Bot will send a percentage of what a great pair people think the match is!', inline: true },
